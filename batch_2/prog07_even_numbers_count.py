ef43a74baaa3770f0b2b812e46a19d8ff3c4d942
Chariz <santoscharizkyle@gmail.com>
2025-03-13 01:32:21 +0800

    increment even count and print even numbers

--- batch_2/prog07_even_numbers_count.py
+++ batch_2/prog07_even_numbers_count.py
@@ -5,5 +5,10 @@ even_numbers = []
 for i in range(10):
     num = float(input(f"Enter number {i+1}: "))
 
-# if the number is even, increment even_count and add the number to even_numbers list
-# display the count of even numbers and the list of even numbers
+    if num % 2 == 0:
+        even_count += 1
+        even_numbers.append(num)
+
+print()
+print(even_count, "even numbers")
+print(even_numbers)
