4b588857f94c30a1199303996c5c759f55682b01
Chariz <santoscharizkyle@gmail.com>
2025-03-12 23:07:55 +0800

    print output

--- batch 1/prog01_bigger_number.py
+++ batch 1/prog01_bigger_number.py
@@ -2,5 +2,9 @@ print("Program 1: Bigger Number between 2 Numbers")
 num1 = float(input("Enter the first number: "))
 num2 = float(input("Enter the second number: "))
 
-# Compare num1 and num2
-# Display bigger number
+if num1 > num2:
+    print("\nThe bigger number is", num1)
+elif num2 > num1:
+    print("\nThe bigger number is", num2)
+else:
+    print("\nThe two numbers are equal.")
