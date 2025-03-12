f3ae3b2759a9417af57df25d09fb7320a672a5e0
Chariz <santoscharizkyle@gmail.com>
2025-03-12 23:55:55 +0800

    pseudocode 7

---
+++ batch_1/prog07_sum_of_10_numbers.py
@@ -0,0 +1,5 @@
+# display program title
+# initialize a variable total to 0 to store the sum
+# use for loop to iterate 10 times to get 10 numbers from the user
+# add the entered number to total
+# display the total sum
