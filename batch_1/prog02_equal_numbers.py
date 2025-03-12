ee7c5787c8927091c4e67f587536c7e2f29b9797
Chariz <santoscharizkyle@gmail.com>
2025-03-12 23:16:30 +0800

    input number

---
+++ batch_1/prog02_equal_numbers.py
@@ -0,0 +1,6 @@
+print("Program 2: Equal Numbers")
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+# check if the two numbers are equal and display "Equal"
+# if not, display "Not Equal"
