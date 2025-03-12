bf59cac4e6ae0472002f959851cbdd906696cf40
Chariz <santoscharizkyle@gmail.com>
2025-03-12 23:04:54 +0800

    input number

--- batch 1/prog01_bigger_number.py
+++ batch 1/prog01_bigger_number.py
@@ -1,4 +1,6 @@
-# Display program title
-# Input number
+print("Program 1: Bigger Number between 2 Numbers")
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
 # Compare num1 and num2
 # Display bigger number
