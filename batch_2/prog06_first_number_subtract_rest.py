389e2298aed934243f18b89d633ef3ff01c82c49
Chariz <santoscharizkyle@gmail.com>
2025-03-13 01:18:48 +0800

    b2 pseudocode 6

---
+++ batch_2/prog06_first_number_subtract_rest.py
@@ -0,0 +1,6 @@
+# display the prog title
+# initialize an empty list called numbers to store user inputs
+# use for loop to iterate 10 times to get 10 numbers from the user
+# add the number to the numbers list
+# compute the result by subtracting the sum of the rest of the numbers from the first number
+# display the result
