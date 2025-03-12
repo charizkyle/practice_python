0342e98a1b28c5522177ad0ec79743aed38a88da
Chariz <santoscharizkyle@gmail.com>
2025-03-12 23:30:58 +0800

    print product

--- batch_1/prog04_product_of_2_numbers.py
+++ batch_1/prog04_product_of_2_numbers.py
@@ -2,5 +2,6 @@ print("Program 4: Product of 2 Numbers")
 num1 = float(input("Enter the first number: "))
 num2 = float(input("Enter the second number: "))
 
-# compute the product of num1 and num2, and store it in prod
-# display the product
+prod = num1 * num2
+print()
+print(prod)
