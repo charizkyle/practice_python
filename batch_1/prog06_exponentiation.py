efd64ad22cf3a4d7c33a19be43e6ce3d3fa9dfa2
Chariz <santoscharizkyle@gmail.com>
2025-03-12 23:51:26 +0800

    print result

--- batch_1/prog06_exponentiation.py
+++ batch_1/prog06_exponentiation.py
@@ -2,5 +2,6 @@ print("Program 6: Exponentiation")
 base = float(input("Enter the base number: "))
 exponent = float(input("Enter the exponent: "))
 
-# compute the result of base raised to the power of exponent
-# display the result
+result = base ** exponent
+print()
+print(result)
