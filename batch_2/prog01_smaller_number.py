416199ca97342218023ab1014a2c7d1daac549e2
Chariz <santoscharizkyle@gmail.com>
2025-03-13 00:41:50 +0800

    input and print smaller number

--- batch_2/prog01_smaller_number.py
+++ batch_2/prog01_smaller_number.py
@@ -1,4 +1,10 @@
-# display program title
-# input 2 numbers
-# compare the 2 numbers
-# display the smaller number
+print("Program 1: Smaller Number between 2 Numbers")
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+if num1 < num2:
+    print("\nThe smaller number is", num1)
+elif num2 < num1:
+    print("\nThe smaller number is", num2)
+else:
+    print("\nThe two numbers are equal.")
