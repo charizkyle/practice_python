7a3515440dd851ae7a71b3907d98d50ba01471d7
Chariz <santoscharizkyle@gmail.com>
2025-03-12 23:49:52 +0800

    input base and exponent

--- batch_1/prog06_exponentiation.py
+++ batch_1/prog06_exponentiation.py
@@ -1,4 +1,6 @@
-# display prog title
-# input base and exponent
+print("Program 6: Exponentiation")
+base = float(input("Enter the base number: "))
+exponent = float(input("Enter the exponent: "))
+
 # compute the result of base raised to the power of exponent
 # display the result
