d37b86496d4e483ea9d79bb102058d63cd227724
Chariz <santoscharizkyle@gmail.com>
2025-03-13 01:23:01 +0800

    result

--- batch_2/prog06_first_number_subtract_rest.py
+++ batch_2/prog06_first_number_subtract_rest.py
@@ -4,5 +4,6 @@ for i in range(10):
     num = float(input(f"Enter number {i+1}: "))
     numbers.append(num)
 
-# compute the result by subtracting the sum of the rest of the numbers from the first number
-# display the result
+result = numbers[0] - sum(numbers[1:])
+print()
+print(result)
