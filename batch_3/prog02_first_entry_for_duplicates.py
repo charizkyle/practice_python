fce07cbf80453e4170cfa6cc989923f992337c2c
Chariz <santoscharizkyle@gmail.com>
2025-03-13 02:23:15 +0800

    b3 pseudocode 2

---
+++ batch_3/prog02_first_entry_for_duplicates.py
@@ -0,0 +1,5 @@
+# display program title
+# initialize an empty list
+# use a for loop to get 10 numbers from the user
+# append the number only if it is not already in the list
+# display the first entry for each duplicate number
