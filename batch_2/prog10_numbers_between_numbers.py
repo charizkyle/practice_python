8e7c01e1e67ba8bb42163e8e70882a87812dccd4
Chariz <santoscharizkyle@gmail.com>
2025-03-13 01:47:36 +0800

    print numbers in the range

--- batch_2/prog10_numbers_between_numbers.py
+++ batch_2/prog10_numbers_between_numbers.py
@@ -2,6 +2,6 @@ print("Program 10: Numbers between numbers")
 num1 = int(input("Enter the first number: "))
 num2 = int(input("Enter the second number: "))
 
-# display a message indicating the range of numbers between the two numbers
-# use a for loop to display the numbers between the two numbers
-# print each number in the range
+print("Numbers between", num1, "and", num2, "are:")
+for num in range(num1 + 1, num2):
+    print(num)
