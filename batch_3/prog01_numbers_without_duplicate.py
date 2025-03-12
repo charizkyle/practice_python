8b2528243c11d0a7713c14d9c7e85d093ee28c2f
Chariz <santoscharizkyle@gmail.com>
2025-03-13 01:56:37 +0800

    b3 pseudocode 1

---
+++ batch_3/prog01_numbers_without_duplicate.py
@@ -0,0 +1,5 @@
+# display program title
+# initialize an empty list to store numbers
+# use a loop to get 10 numbers from the user
+# find numbers that are not duplicates
+# display the numbers without duplicates
