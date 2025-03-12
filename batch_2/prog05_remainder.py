4318f90fdc699fc2b470817d2b57f203cabe7365
Chariz <santoscharizkyle@gmail.com>
2025-03-13 01:14:03 +0800

    b2 pseudocode 5

---
+++ batch_2/prog05_remainder.py
@@ -0,0 +1,5 @@
+# display the prog title
+# input two numbers
+# if num2 is zero, print "Division by zero is UNDEFINED."
+# else, calculate the remainder of num1 divided by num2
+# display the remainder
