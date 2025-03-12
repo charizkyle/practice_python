6ec4cb9cb65c64c1d3b0a7ecc07eb2ec1effe85e
Chariz <santoscharizkyle@gmail.com>
2025-03-12 23:39:13 +0800

    input number and compute

--- batch_1/prog05_quotient_with_decimal.py
+++ batch_1/prog05_quotient_with_decimal.py
@@ -1,4 +1,10 @@
-# display the program title
-# input 2 numbers
-# if num2 is zero, display "Division by zero is UNDEFINED."
-# else, compute the quotient of num1 and num2, and store it in quotient
+print("Program 5: Quotient of 2 Numbers")
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+if num2 == 0:
+    print("\nDivision by zero is UNDEFINED.")
+else:
+    quotient = num1 / num2
+    print()
+    print(quotient)
