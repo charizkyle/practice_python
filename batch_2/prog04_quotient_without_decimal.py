7de50b16af3f7a54e5a68e40daa03dbaeae5a7ed
Chariz <santoscharizkyle@gmail.com>
2025-03-13 01:08:00 +0800

    division by zero is undefined

--- batch_2/prog04_quotient_without_decimal.py
+++ batch_2/prog04_quotient_without_decimal.py
@@ -1,5 +1,9 @@
-# display prog title
-# input two numbers
-# if second number is zero, print division by zero is undefined
+print("Program 4: Quotient of 2 Numbers without Decimal")
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+if num2 == 0:
+    print("\nDivision by zero is UNDEFINED.")
+
 # else, calculate quotient, convert it to an integer to remove decimals, and store it in quotient
 # print the quotient
