33b17241e0ca7df6161972d054ddb5d1819c075b
Chariz <santoscharizkyle@gmail.com>
2025-03-13 01:36:56 +0800

    print the odd numbers

--- batch_2/prog08_odd_numbers_0-100.py
+++ batch_2/prog08_odd_numbers_0-100.py
@@ -1,5 +1,5 @@
-# display prog title
-# initialize variable num to 1 (first odd number)
-# use a while loop to iterate while num is less than or equal to 100
-# print the current odd number
-# increment num by 2 to get the next odd number
+print("Program 8: Odd Numbers starting from 0-100")
+num = 1
+while num <= 100:
+    print(num)
+    num += 2
