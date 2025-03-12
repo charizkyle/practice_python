0266d38304644f41fdcf8fda93171a91084d673d
Chariz <santoscharizkyle@gmail.com>
2025-03-13 01:05:00 +0800

    b2 pseudocode 4

---
+++ batch_2/prog04_quotient_without_decimal.py
@@ -0,0 +1,5 @@
+# display prog title
+# input two numbers
+# if second number is zero, print division by zero is undefined
+# else, calculate quotient, convert it to an integer to remove decimals, and store it in quotient
+# print the quotient
