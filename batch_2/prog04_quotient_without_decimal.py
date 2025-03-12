75d3f4e6a68b71d1fa44cac3b82d017d5b0ffc9f
Chariz <santoscharizkyle@gmail.com>
2025-03-13 01:09:36 +0800

    print int quotient

--- batch_2/prog04_quotient_without_decimal.py
+++ batch_2/prog04_quotient_without_decimal.py
@@ -4,6 +4,7 @@ num2 = float(input("Enter the second number: "))
 
 if num2 == 0:
     print("\nDivision by zero is UNDEFINED.")
-
-# else, calculate quotient, convert it to an integer to remove decimals, and store it in quotient
-# print the quotient
+else:
+    quotient = int(num1 / num2)
+    print()
+    print(quotient)
