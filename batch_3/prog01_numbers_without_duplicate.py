8ee2f3445e3840e3b99fa69e7daa3401bdf0831a
Chariz <santoscharizkyle@gmail.com>
2025-03-13 02:03:25 +0800

    initializa empty list and for loop input

--- batch_3/prog01_numbers_without_duplicate.py
+++ batch_3/prog01_numbers_without_duplicate.py
@@ -1,5 +1,9 @@
-# display program title
-# initialize an empty list to store numbers
+print("Program 1: Numbers without duplicates")
+numbers = []
+for i in range(10):
+    num = float(input(f"Enter number {i+1}: "))
+    numbers.append(num)
+
 # use a loop to get 10 numbers from the user
 # find numbers that are not duplicates
 # display the numbers without duplicates
