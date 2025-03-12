53b78c2d98030b696fcac57d6a2e9f29b53c98a9
Chariz <santoscharizkyle@gmail.com>
2025-03-12 23:23:56 +0800

    input num and print sum

--- batch_1/prog03_sum_of_2_numbers.py
+++ batch_1/prog03_sum_of_2_numbers.py
@@ -1,3 +1,7 @@
-# Display program title
-# input number
-# Compute the sum of num1 and num2, and store it in sum
+print("Program 3: Sum of 2 Numbers")
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+sum = num1 + num2
+print()
+print(sum)
