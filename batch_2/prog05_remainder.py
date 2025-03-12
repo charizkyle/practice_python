9399dd2772fbf4bce0f4a7ab2ffcff5844234dc3
Chariz <santoscharizkyle@gmail.com>
2025-03-13 01:15:56 +0800

    display remainder

--- batch_2/prog05_remainder.py
+++ batch_2/prog05_remainder.py
@@ -1,5 +1,10 @@
-# display the prog title
-# input two numbers
-# if num2 is zero, print "Division by zero is UNDEFINED."
-# else, calculate the remainder of num1 divided by num2
-# display the remainder
+print("Program 5: Remainder")
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+if num2 == 0:
+    print("\nDivision by zero is UNDEFINED.")
+else:
+    rem = num1 % num2
+    print()
+    print(rem)
