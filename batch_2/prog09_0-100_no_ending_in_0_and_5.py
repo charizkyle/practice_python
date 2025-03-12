452d780ee8491b7e28c3bb7303f98ade513b7ce5
Chariz <santoscharizkyle@gmail.com>
2025-03-13 01:39:55 +0800

    b2 pseudocode 9

---
+++ batch_2/prog09_0-100_no_ending_in_0_and_5.py
@@ -0,0 +1,5 @@
+# display program title
+# initialize a variable num to 0
+# while num is less than or equal to 100
+# if num is not divisible by 10 and 5 (num % 10 != 0 and num % 5 != 0)
+# print num
