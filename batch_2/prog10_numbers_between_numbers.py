aa6cf8fa3f925d64900ab1b87229458b67798117
Chariz <santoscharizkyle@gmail.com>
2025-03-13 01:49:06 +0800

    ensure num1 is smaller

--- batch_2/prog10_numbers_between_numbers.py
+++ batch_2/prog10_numbers_between_numbers.py
@@ -2,6 +2,10 @@ print("Program 10: Numbers between numbers")
 num1 = int(input("Enter the first number: "))
 num2 = int(input("Enter the second number: "))
 
+# ensure num1 is the smaller number by swapping values if num1 is greater than num2
+if num1 > num2:
+    num1, num2 = num2, num1
+
 print("Numbers between", num1, "and", num2, "are:")
 for num in range(num1 + 1, num2):
     print(num)
