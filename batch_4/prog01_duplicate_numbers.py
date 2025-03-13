aa3e33dc6dc4551a77571ec26630cf3dddc5591b
Chariz <santoscharizkyle@gmail.com>
2025-03-13 12:27:25 +0800

    for loop input

--- batch_4/prog01_duplicate_numbers.py
+++ batch_4/prog01_duplicate_numbers.py
@@ -1,6 +1,11 @@
 # display the program title
 print("Program 1: Duplicate Numbers")
 numbers = []
+
+for i in range(10):
+    num = float(input(f"Enter number {i+1}: "))
+    numbers.append(num)
+    
 # use a for loop to get 10 numbers from the user
 # find and display the duplicate numbers
 # display the result
