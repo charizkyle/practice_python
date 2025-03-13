6b788672cb95bb549ef261540ad3287aacdf2977
Chariz <santoscharizkyle@gmail.com>
2025-03-13 12:06:29 +0800

    b3 pseudocode 5

---
+++ batch_3/prog05_lowest_to_highest.py
@@ -0,0 +1,8 @@
+# display the program title
+# initialize an empty list to store the numbers
+# continuously ask the user to enter a number until an invalid input is entered
+#     prompt the user to enter a number
+#     if the input is not a number, exit the loop
+#     if the input is a number, add it to the list
+# sort the list in ascending order
+# display the sorted list
