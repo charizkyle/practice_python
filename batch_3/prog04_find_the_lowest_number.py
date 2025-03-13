b992ee93cdba68bcd1169665d8831b70ecb41c65
Chariz <santoscharizkyle@gmail.com>
2025-03-13 11:29:59 +0800

    b3 pseudocode 4

---
+++ batch_3/prog04_find_the_lowest_number.py
@@ -0,0 +1,6 @@
+# display program title
+# initialize a variable to store the lowest number (set None as the initial value)
+# continuously ask the user for input until an invalid input is entered
+# ask the user for a number
+# update the lowest number if the entered number is less than the current lowest number
+# stop the loop if an invalid input or non-numeric value is entered
