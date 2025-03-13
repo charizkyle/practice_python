1a529f6981eb78c571a4a0d4fc23ba0e6a72986e
Chariz <santoscharizkyle@gmail.com>
2025-03-13 13:29:55 +0800

    b4 pseudocode 5

---
+++ batch_4/prog05_average.py
@@ -0,0 +1,7 @@
+# display the program title
+# initialize a list to store numbers
+# continuously ask the user to enter a number until an invalid input is entered
+#   add the number to the list
+#   if an invalid input is entered, break the loop
+# calculate the average of the numbers in the list
+# display the average
