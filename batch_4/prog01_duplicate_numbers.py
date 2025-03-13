085303bfbf59ef790a71d4a220a0588805c0d441
Chariz <santoscharizkyle@gmail.com>
2025-03-13 12:20:48 +0800

    b4 pseudocde 1

---
+++ batch_4/prog01_duplicate_numbers.py
@@ -0,0 +1,5 @@
+# display the program title
+# initialize an empty list to store the numbers
+# use a for loop to get 10 numbers from the user
+# find and display the duplicate numbers
+# display the result
