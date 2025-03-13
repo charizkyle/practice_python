196b0369e04ef60edaad3847faecea54c0b0adbb
Chariz <santoscharizkyle@gmail.com>
2025-03-13 10:56:22 +0800

    detect duplicate and unique numbers and print

--- batch_3/prog03_detect_unique_and_duplicate_numbers.py
+++ batch_3/prog03_detect_unique_and_duplicate_numbers.py
@@ -1,10 +1,15 @@
 print("Program 3: Detect Unique and Duplicate Numbers")
 numbers = []
-# continuously ask the user for input until an invalid input is entered
+
 while True:
     try:
         num = float(input("Enter a number: "))
 
-# check if the numbers is a duplicate
-# else if it is unique, add it to the list
-# if the input is invalid, break the loop
+        if num in numbers:
+            print("Duplicate")
+        else:
+            print("Unique")
+            numbers.append(num)
+    except ValueError:
+        print("Invalid input. Exiting the program.")
+        break
