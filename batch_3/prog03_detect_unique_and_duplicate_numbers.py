60588a49dab1f7629d40b850d7d5ba2963b7caeb
Chariz <santoscharizkyle@gmail.com>
2025-03-13 10:46:57 +0800

    continuous input until invalid input occurs

--- batch_3/prog03_detect_unique_and_duplicate_numbers.py
+++ batch_3/prog03_detect_unique_and_duplicate_numbers.py
@@ -1,7 +1,10 @@
-# display the program title
-# initialize an empty list to store the numbers
+print("Program 3: Detect Unique and Duplicate Numbers")
+numbers = []
 # continuously ask the user for input until an invalid input is entered
-# ask for a  number
+while True:
+    try:
+        num = float(input("Enter a number: "))
+
 # check if the numbers is a duplicate
 # else if it is unique, add it to the list
 # if the input is invalid, break the loop
