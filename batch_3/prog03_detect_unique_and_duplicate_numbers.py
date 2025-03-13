305c1cb267d0320f0f6ff5d753da4a1ad6668351
Chariz <santoscharizkyle@gmail.com>
2025-03-13 10:40:29 +0800

    b3 pseudocode 3

---
+++ batch_3/prog03_detect_unique_and_duplicate_numbers.py
@@ -0,0 +1,7 @@
+# display the program title
+# initialize an empty list to store the numbers
+# continuously ask the user for input until an invalid input is entered
+# ask for a  number
+# check if the numbers is a duplicate
+# else if it is unique, add it to the list
+# if the input is invalid, stop the loop
