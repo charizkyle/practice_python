4637462abf0f24a5f1b9b7566a5a3037713aaf3b
Chariz <santoscharizkyle@gmail.com>
2025-03-13 13:35:51 +0800

    input

--- batch_4/prog05_average.py
+++ batch_4/prog05_average.py
@@ -1,8 +1,12 @@
 print("Calculate the Average")
 numbers = []
 
-# continuously ask the user to enter a number until an invalid input is entered
-#   add the number to the list
-#   if an invalid input is entered, break the loop
+while True:
+    try:
+        num = float(input("Enter a number or press Enter to stop: "))
+
+        numbers.append(num)
+    except ValueError:
+        break
 # calculate the average of the numbers in the list
 # display the average
