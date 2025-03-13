f1e2ce8e429508472d7fd208e401074eb55269c6
Chariz <santoscharizkyle@gmail.com>
2025-03-13 12:50:01 +0800

    finding the most frequent number

--- batch_4/prog02_frequent_number.py
+++ batch_4/prog02_frequent_number.py
@@ -8,4 +8,12 @@ while True:
     except ValueError:
         break
 # find the most frequent number in the list or a number with most duplicates
+if numbers:
+    most_frequent = None
+    most_frequent_count = 0
+    for num in numbers:
+        count = numbers.count(num)
+        if count > most_frequent_count:
+            most_frequent = num
+            most_frequent_count = count
 # display the most frequent number
