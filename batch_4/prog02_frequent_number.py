f61244080d049101300f129aead885c0c7315d80
Chariz <santoscharizkyle@gmail.com>
2025-03-13 12:46:40 +0800

    input numbers

--- batch_4/prog02_frequent_number.py
+++ batch_4/prog02_frequent_number.py
@@ -1,5 +1,11 @@
 print("Program 2: Most Frequent Number")
 numbers = []
-# continuously ask the user to enter a number until a non-numeric value is entered to exit the program
+
+while True:
+    try:
+        num = float(input("Enter a number or press Enter to stop: "))
+        numbers.append(num)
+    except ValueError:
+        break
 # find the most frequent number in the list or a number with most duplicates
 # display the most frequent number
