1a89bf741aa2e22efb5b1a3a76f531a9ad0dc85e
Chariz <santoscharizkyle@gmail.com>
2025-03-13 12:41:55 +0800

    b4 pseudocode 2

---
+++ batch_4/prog02_frequent_number.py
@@ -0,0 +1,5 @@
+# display the program title
+# initialize an empty list
+# continuously ask the user to enter a number until a non-numeric value is entered to exit the program
+# find the most frequent number in the list or a number with most duplicates
+# display the most frequent number
