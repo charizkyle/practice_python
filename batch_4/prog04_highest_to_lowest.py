eb1fea51fef814277a9bc6ed03413fd9f617af3f
Chariz <santoscharizkyle@gmail.com>
2025-03-13 13:16:55 +0800

    input number

--- batch_4/prog04_highest_to_lowest.py
+++ batch_4/prog04_highest_to_lowest.py
@@ -1,5 +1,12 @@
 print("Program 4: Sort Numbers in Descending Order")
 numbers = []
-# continuously ask the user to enter a number until a non-numeric value is entered
+
+while True:
+    try:
+        num = float(input("Enter a number or press Enter to stop: "))
+
+        numbers.append(num)
+    except ValueError:
+        break
 # if the list is not empty, sort the numbers in descending order
 # display the numbers from highest to lowest
