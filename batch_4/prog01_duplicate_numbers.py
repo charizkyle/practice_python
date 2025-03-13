a3697d3d8ec7ddd13866bf083c254177e3730d39
Chariz <santoscharizkyle@gmail.com>
2025-03-13 12:24:16 +0800

    initialize

--- batch_4/prog01_duplicate_numbers.py
+++ batch_4/prog01_duplicate_numbers.py
@@ -1,5 +1,6 @@
 # display the program title
-# initialize an empty list to store the numbers
+print("Program 1: Duplicate Numbers")
+numbers = []
 # use a for loop to get 10 numbers from the user
 # find and display the duplicate numbers
 # display the result
