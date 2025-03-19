b97f5527b31108ff5ef279727767a1ea6698dfad
Chariz <santoscharizkyle@gmail.com>
2025-03-19 16:22:23 +0800

    b5 pseudocode 7

---
+++ batch_5/prog07_number_of_words.py
@@ -0,0 +1,4 @@
+# display the program's title
+# ask the user to input a complete statement
+# split the statement into words and count the number of words
+# print the number of words in the statement
