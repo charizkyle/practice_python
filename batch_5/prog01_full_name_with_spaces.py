0478267c5f191d9d382533c41b35e7f40d79942f
Chariz <santoscharizkyle@gmail.com>
2025-03-19 12:39:03 +0800

    b5 pseudocode 1

---
+++ batch_5/prog01_full_name_with_spaces.py
@@ -0,0 +1,5 @@
+# display the program's title
+# program to remove leading spaces from the user's full name
+# ask the user to input their full name with leading spaces
+# remove leading spaces
+# print the name without leading spaces
