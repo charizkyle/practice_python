300c00e413aceb02c39da2dc65e77961678dea72
Chariz <santoscharizkyle@gmail.com>
2025-03-19 16:33:56 +0800

    b5 pseudocode 8

---
+++ batch_5/prog08_fullname_number_of_characters.py
@@ -0,0 +1,4 @@
+# display the program's title
+# ask the user to input their full name
+# count the number of characters in the full name (including spaces)
+# print the number of characters in the full name
