3f9facaa458f062e0f4059b2bd4ffdb2e7674b16
Chariz <santoscharizkyle@gmail.com>
2025-03-19 15:43:01 +0800

    b5 pseudocode 3

---
+++ batch_5/prog03_fullname_in_capital_letters.py
@@ -0,0 +1,4 @@
+# display the program's title
+# program to ask the user to input their full name and print it in all capital letters
+# ask the user to input their full name
+# print the full name in all capital letters
