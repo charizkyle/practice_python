ab625c8b0678c4e30a02abef34119973dacbeaf2
Chariz <santoscharizkyle@gmail.com>
2025-03-19 16:28:38 +0800

    input a statement

--- batch_5/prog07_number_of_words.py
+++ batch_5/prog07_number_of_words.py
@@ -1,4 +1,8 @@
 # display the program's title
+print("Program 7: Number of Words in a Statement")
+
 # ask the user to input a complete statement
+statement = input("Enter a complete statement: ")
+
 # split the statement into words and count the number of words
 # print the number of words in the statement
