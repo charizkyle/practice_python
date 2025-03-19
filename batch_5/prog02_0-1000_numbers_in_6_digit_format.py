abebc15a33e1077b250eb0fd4143c516d9afdb0b
Chariz <santoscharizkyle@gmail.com>
2025-03-19 14:56:34 +0800

    num input

--- batch_5/prog02_0-1000_numbers_in_6_digit_format.py
+++ batch_5/prog02_0-1000_numbers_in_6_digit_format.py
@@ -1,4 +1,8 @@
 # display the program's title
+print("Program 2: Format Numbers in 6 Digits")
+
 # ask the user for a number between 0 and 1000
+number = int(input("Enter a number between 0 and 1000: "))
+
 # format the number to have 6 digits
 # print the formatted number
