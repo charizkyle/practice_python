9c6b890036626459814f252c8a51a08e76c9e23c
Chariz <santoscharizkyle@gmail.com>
2025-03-19 16:06:05 +0800

    b5 pseudocode 6

---
+++ batch_5/prog06_fullname_reverse_casing.py
@@ -0,0 +1,4 @@
+# display the program's title
+# ask the user to input their full name with incorrect casing
+# reverse the caasing of each character in the full name
+# print the full name with reversed casing
