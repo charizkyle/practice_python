91914ea42a46aa217315ab45f9068d360e700698
Chariz <santoscharizkyle@gmail.com>
2025-03-19 16:37:23 +0800

    count number of characters

--- batch_5/prog08_fullname_number_of_characters.py
+++ batch_5/prog08_fullname_number_of_characters.py
@@ -1,4 +1,11 @@
 # display the program's title
+print("Program 8: Number of Characters in Full Name")
+
 # ask the user to input their full name
+full_name = input("Enter your full name: ")
+
 # count the number of characters in the full name (including spaces)
+number_of_characters = len(full_name)
+
 # print the number of characters in the full name
+print("\nNumber of characters in full name:", number_of_characters)
