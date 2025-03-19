b3b66cad7bd5ebc43797a7d42e1749bb9288b334
Chariz <santoscharizkyle@gmail.com>
2025-03-19 15:59:30 +0800

    print name in proper casing

--- batch_5/prog05_fullname_proper_casing.py
+++ batch_5/prog05_fullname_proper_casing.py
@@ -1,3 +1,8 @@
 # display the program's title
+print("Program 5: Full Name in Proper Casing")
+
 # ask the user to input their full name in incorrect casing
+fullname = input("Enter your full name in incorrect casing: ")
+
 # convert and print the input to proper casing
+print(f"\nFull name in proper casing: {fullname.title()}")
