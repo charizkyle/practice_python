1fbe1d1d318afff40292dc2b30748e1f9edd4469
Chariz <santoscharizkyle@gmail.com>
2025-03-19 15:51:26 +0800

    print name in lower case

---
+++ batch_5/prog04_fullname_in_lower_case.py
@@ -0,0 +1,8 @@
+# display the program's title
+print("Program 3: Full Name in Capital Letters")
+
+# ask the user to input their full name
+full_name = input("Enter your full name: ")
+
+# print the full name in all capital letters
+print(f"\nFull name in lower case letters: {full_name.lower()}")
