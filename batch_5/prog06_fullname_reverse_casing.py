0a86fc2861bdb90809dbbc0c05d4a82c81cda3f8
Chariz <santoscharizkyle@gmail.com>
2025-03-19 16:19:28 +0800

    print name in reversed casing

--- batch_5/prog06_fullname_reverse_casing.py
+++ batch_5/prog06_fullname_reverse_casing.py
@@ -1,4 +1,11 @@
 # display the program's title
+print("Program 6: Full Name with Reversed Casing")
+
 # ask the user to input their full name with incorrect casing
+full_name = input("Enter your full name with incorrect casing: ")
+
 # reverse the caasing of each character in the full name
+reversed = "".join(char.upper()if char.islower() else char.lower() for char in full_name)
+
 # print the full name with reversed casing
+print("\nFull name with reversed casing", reversed)
