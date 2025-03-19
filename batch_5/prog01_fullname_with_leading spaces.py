4f7287a8374101e47f87d903a2ab8697a2a15b7f
Chariz <santoscharizkyle@gmail.com>
2025-03-19 12:51:56 +0800

    print without leading spaces

--- batch_5/prog01_fullname_with_leading spaces.py
+++ batch_5/prog01_fullname_with_leading spaces.py
@@ -1,9 +1,11 @@
 # display the program's title
 print("Program 1: Remove Leading Spaces from Full Name")
 
-# program to remove leading spaces from the user's full name
 # ask the user to input their full name with leading spaces
 name = input("Enter your full name with leading spaces: ")
 
 # remove leading spaces
+name = name.lstrip()
+
 # print the name without leading spaces
+print(f"\nName without leading spaces: {name}")
