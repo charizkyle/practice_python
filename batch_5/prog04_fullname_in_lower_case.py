fad05716e5aef8f812d6f22676083837e54e6a79
Chariz <santoscharizkyle@gmail.com>
2025-03-22 06:25:38 +0800

    small changes for some of the programs

--- batch_5/prog04_fullname_in_lower_case.py
+++ batch_5/prog04_fullname_in_lower_case.py
@@ -1,8 +1,8 @@
 # display the program's title
-print("Program 4: Full Name in Lower Case Letters")
+print("Program 4: Full Name in Lowercase Letters")
 
 # ask the user to input their full name
 full_name = input("Enter your full name: ")
 
 # print the full name in all capital letters
-print(f"\nFull name in lower case letters: {full_name.lower()}")
+print(f"\nFull name in lowercase letters: {full_name.lower()}")
--- batch_5/prog05_fullname_proper_casing.py
+++ batch_5/prog05_fullname_proper_casing.py
@@ -2,7 +2,7 @@
 print("Program 5: Full Name in Proper Casing")
 
 # ask the user to input their full name in incorrect casing
-fullname = input("Enter your full name in incorrect casing: ")
+full_name = input("Enter your full name in incorrect casing: ")
 
 # convert and print the input to proper casing
-print(f"\nFull name in proper casing: {fullname.title()}")
+print(f"\nFull name in proper casing: {full_name.title()}")
