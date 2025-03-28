77f96119c8a6a25bff4b9e7c86b7773596864b9e
Chariz <santoscharizkyle@gmail.com>
2025-03-28 23:35:22 +0800

    add spaces to the right until it reaches the target length

--- batch_6/prog06_string_right_spacer_without_using_ljust().py
+++ batch_6/prog06_string_right_spacer_without_using_ljust().py
@@ -3,8 +3,14 @@ print("Program 6: String Right Spacer without using ljust()")
 
 # ask the user for a string and a target length
 string = input("Enter a string: ")
+target_length = int(input("Enter the target length: "))
 
 # add spaces to the right of the string until it reaches the target length
-#   truncate the string if it is already longer than the target length
-#   keep it unchanged if it is equal to the target length
+if len(string) < target_length:
+    string += " " * (target_length - len(string))
+else:
+    string = string[:target_length] # truncate the string if it is longer than the target length
+
 # print the result
+print()
+print(f"""'{string}'""")
