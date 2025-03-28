2626547aea0abe41cbd3fb08d5afbbdb0d5f4b0f
Chariz <santoscharizkyle@gmail.com>
2025-03-28 23:00:45 +0800

    ask the user for a string and the suffix to check

--- batch_6/prog05_string_suffix_checker_without_using_endswith().py
+++ batch_6/prog05_string_suffix_checker_without_using_endswith().py
@@ -1,4 +1,9 @@
 # display the program's title
+print("Program 5: String Suffix Checker without using endswith()")
+
 # ask the user for a string and a suffix to check
+string = input("Enter a string: ")
+suffix = input("Enter the suffix to check: ")
+
 # check if the string ends with the given suffix
 # print the result
