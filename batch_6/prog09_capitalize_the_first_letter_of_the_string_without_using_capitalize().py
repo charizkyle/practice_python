513220c37b1e6119de5e5fef81c692677ea88e3e
Chariz <santoscharizkyle@gmail.com>
2025-03-29 00:23:53 +0800

    print in title casing

--- batch_6/prog09_capitalize_the_first_letter_of_the_string_without_using_capitalize().py
+++ batch_6/prog09_capitalize_the_first_letter_of_the_string_without_using_capitalize().py
@@ -1,3 +1,9 @@
 # display the program's title
+print("Program 9: Capitalize the First Letter of the String without using capitalize()")
+
 # ask the user for a string
+string = input("Enter a string: ")
+
 # print the first letter of the string in capitalized form
+print()
+print(string.title())
