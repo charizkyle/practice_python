2539614447d508d3d036d2a178618a0013661b7e
Chariz <santoscharizkyle@gmail.com>
2025-03-29 00:12:51 +0800

    reverse the casing of each character in the string

--- batch_6/prog08_reverse_casing_without_using_swapcase().py
+++ batch_6/prog08_reverse_casing_without_using_swapcase().py
@@ -5,4 +5,8 @@ print("Program 8: Reverse Casing of String without using swapcase()")
 string = input("Enter a string: ")
 
 # reverse the casing of each character in the string
+reversed = "".join(char.upper() if char.islower() else char.lower() for char in string)
+
 # print the result
+print()
+print(reversed)
