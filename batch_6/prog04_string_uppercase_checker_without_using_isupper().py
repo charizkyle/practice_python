a6d4909fb7c4b92214500700ecafd243128e2f8f
Chariz <santoscharizkyle@gmail.com>
2025-03-28 22:42:51 +0800

    input string

--- batch_6/prog04_string_uppercase_checker_without_using_isupper().py
+++ batch_6/prog04_string_uppercase_checker_without_using_isupper().py
@@ -1,4 +1,8 @@
 # display the program's title
+print("Program 4: String Uppercase Checker without using isupper()")
+
 # ask the user for a string
+string = input("Enter a string: ")
+
 # convert the string to uppercase and compare it with the original
 # print the result
