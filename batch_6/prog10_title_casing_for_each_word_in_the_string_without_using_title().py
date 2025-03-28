34c782b83fdf450ccccf2094c78b401a5839585d
Chariz <santoscharizkyle@gmail.com>
2025-03-29 00:47:49 +0800

    apply title casing to each word

--- batch_6/prog10_title_casing_for_each_word_in_the_string_without_using_title().py
+++ batch_6/prog10_title_casing_for_each_word_in_the_string_without_using_title().py
@@ -5,4 +5,8 @@ print("Program 10: Title Casing for Each Word in the String without using title(
 string = input("Enter a string: ")
 
 # split the string into words and apply title casing to each word
+result = " ".join(word.capitalize() for word in string.split())
+
 # print the result
+print()
+print(result)
