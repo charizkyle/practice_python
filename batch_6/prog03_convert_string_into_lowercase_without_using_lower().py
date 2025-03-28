d2a6ac1f9c4d3f7ac066ca60cab8dd9a2192e766
Chariz <santoscharizkyle@gmail.com>
2025-03-28 22:15:46 +0800

    input string

--- batch_6/prog03_convert_string_into_lowercase_without_using_lower().py
+++ batch_6/prog03_convert_string_into_lowercase_without_using_lower().py
@@ -1,4 +1,8 @@
 # display the program's title
+print("Program 3: Convert String into Lowercase without using lower()")
+
 # input string
+string = input("Enter a string: ")
+
 # use str.casefold() to convert to lowercase (alternative to lower())
 # print the string in lowercase
