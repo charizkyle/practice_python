f00ae43feecee6b4311edf2da9e7776e4c743c06
Chariz <santoscharizkyle@gmail.com>
2025-03-28 23:22:17 +0800

    input string

--- batch_6/prog06_string_right_spacer_without_using_ljust().py
+++ batch_6/prog06_string_right_spacer_without_using_ljust().py
@@ -1,5 +1,9 @@
 # display the program's title
+print("Program 6: String Right Spacer without using ljust()")
+
 # ask the user for a string and a target length
+string = input("Enter a string: ")
+
 # add spaces to the right of the string until it reaches the target length
 #   truncate the string if it is already longer than the target length
 #   keep it unchanged if it is equal to the target length
