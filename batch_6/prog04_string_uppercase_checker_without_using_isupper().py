0c60695b6b6aeb75492b7b04581ac71a94bf895b
Chariz <santoscharizkyle@gmail.com>
2025-03-28 22:45:27 +0800

    check if the string is in uppercase and print the result

--- batch_6/prog04_string_uppercase_checker_without_using_isupper().py
+++ batch_6/prog04_string_uppercase_checker_without_using_isupper().py
@@ -5,4 +5,12 @@ print("Program 4: String Uppercase Checker without using isupper()")
 string = input("Enter a string: ")
 
 # convert the string to uppercase and compare it with the original
+uppercase_string = string.upper()
+if string == uppercase_string:
+    result = "The string is uppercase."
+else:
+    result = "The string is not uppercase."
+    
 # print the result
+print()
+print(result)
