497b21c65545ed14c27b66ce188f87b387588843
Chariz <santoscharizkyle@gmail.com>
2025-03-28 21:34:02 +0800

    input string

--- batch_6/prog01_string_without_leading_spaces.py
+++ batch_6/prog01_string_without_leading_spaces.py
@@ -1,4 +1,8 @@
 # display the program title
+print("Program 1: Remove Leading Spaces from String")
+
 # input string with leading spaces
+string = input("Enter a string with leading spaces: ")
+
 # strip each leading spaces using replace()
 # print the string without leading spaces
