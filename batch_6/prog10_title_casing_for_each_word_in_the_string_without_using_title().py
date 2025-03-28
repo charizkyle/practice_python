826529421601f3b99f9033227234c9752941d458
Chariz <santoscharizkyle@gmail.com>
2025-03-29 00:44:56 +0800

    inpiut string

--- batch_6/prog10_title_casing_for_each_word_in_the_string_without_using_title().py
+++ batch_6/prog10_title_casing_for_each_word_in_the_string_without_using_title().py
@@ -1,4 +1,8 @@
 # display the program's title
+print("Program 10: Title Casing for Each Word in the String without using title()")
+
 # ask the user for a string
+string = input("Enter a string: ")
+
 # split the string into words and apply title casing to each word
 # print the result
