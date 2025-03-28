a175b86049627d87c6cd240735b81b8ca5c7b711
Chariz <santoscharizkyle@gmail.com>
2025-03-28 23:46:46 +0800

    ask the user for a string and a target length

--- batch_6/prog07_string_at_center_without_using_center().py
+++ batch_6/prog07_string_at_center_without_using_center().py
@@ -1,4 +1,9 @@
 # display the program's title
+print("Program 7: String at Center without using center()")
+
 # ask the user for a string and a target length
+string = input("Enter a string: ")
+target_length = int(input("Enter the target length: "))
+
 # center the string with spaces on both sides
 # print the result
