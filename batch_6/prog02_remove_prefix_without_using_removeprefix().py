89b7085b96802465dca5136197e5a80bc591bd14
Chariz <santoscharizkyle@gmail.com>
2025-03-28 21:51:59 +0800

    input string and the prefix to remove from the string

--- batch_6/prog02_remove_prefix_without_using_removeprefix().py
+++ batch_6/prog02_remove_prefix_without_using_removeprefix().py
@@ -1,5 +1,10 @@
 # display the program title
+print("Program 2: Remove Prefix from String without using removeprefix()")
+
 # input string and prefix to remove
+string = input("Enter a string: ")
+prefix = input("Enter the prefix to remove: ")
+
 # remove the prefix from the string without using removeprefix()
 #   if no prefix is found, print the original string
 # print the string without the prefix
