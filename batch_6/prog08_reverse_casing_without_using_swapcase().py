f5c1d4d14e2512fd65a4d678af86d47335835d4c
Chariz <santoscharizkyle@gmail.com>
2025-03-29 00:10:58 +0800

    input string

--- batch_6/prog08_reverse_casing_without_using_swapcase().py
+++ batch_6/prog08_reverse_casing_without_using_swapcase().py
@@ -1,4 +1,8 @@
 # display the program's title
+print("Program 8: Reverse Casing of String without using swapcase()")
+
 # ask the user for a string
+string = input("Enter a string: ")
+
 # reverse the casing of each character in the string
 # print the result
