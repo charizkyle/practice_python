945ace77f9729fcdfb5a8be2602cfd77db2fc4f0
Chariz <santoscharizkyle@gmail.com>
2025-03-29 23:01:25 +0800

    ask the user for a string and a substring

--- batch_7/prog10_finding_the_last_location_of_substring_without_using_rindex().py
+++ batch_7/prog10_finding_the_last_location_of_substring_without_using_rindex().py
@@ -1,4 +1,9 @@
 # display the program's title
+print("Program 10: Finding the Last Location of Substring without using rindex()")
+
 # ask the user for a string and a substring
+string = input("Enter a string: ")
+substring = input("Enter the substring to find: ")
+
 # find the last occurrence of the substring using find()
 # if the substring is found, print its index; otherwise, print a message
