049d888b0bd631dd979d0fb1ef924b4c50fd030e
Chariz <santoscharizkyle@gmail.com>
2025-03-29 22:02:10 +0800

    b7 pseudocode 6

---
+++ batch_7/prog06_string_left_spacer_without_using_rjust().py
@@ -0,0 +1,5 @@
+# display the program's title
+# ask the user for a string and a target length
+# add spaces to the left of the string until it reaches the target length
+#   truncate the string if it is longer than the target length
+# print the result
