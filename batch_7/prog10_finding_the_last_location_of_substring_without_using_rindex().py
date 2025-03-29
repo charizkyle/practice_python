e45b031ba0ab235d3fd1759544f3d6253fa0992a
Chariz <santoscharizkyle@gmail.com>
2025-03-29 23:07:38 +0800

    find the last occurrence of the substring

--- batch_7/prog10_finding_the_last_location_of_substring_without_using_rindex().py
+++ batch_7/prog10_finding_the_last_location_of_substring_without_using_rindex().py
@@ -5,5 +5,13 @@ print("Program 10: Finding the Last Location of Substring without using rindex()
 string = input("Enter a string: ")
 substring = input("Enter the substring to find: ")
 
-# find the last occurrence of the substring using find()
+# find the last occurrence of the substring using rfind()
+index = string.rfind(substring)
+
 # if the substring is found, print its index; otherwise, print a message
+if index != -1:
+    print()
+    print(f"The last location of '{substring}' is at index {index}.")
+else:
+    print()
+    print(f"'{substring}' was not found in the string.")
