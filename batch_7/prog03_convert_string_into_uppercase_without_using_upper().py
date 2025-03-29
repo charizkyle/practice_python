b52ff44d3263eba9dbf8e40b440b39bbcc8e65d1
Chariz <santoscharizkyle@gmail.com>
2025-03-29 20:35:16 +0800

    input string in lowercase

--- batch_7/prog03_convert_string_into_uppercase_without_using_upper().py
+++ batch_7/prog03_convert_string_into_uppercase_without_using_upper().py
@@ -1,4 +1,8 @@
 # display the program's title
+print("Program 3: Convert String into Uppercase without using upper()")
+
 # ask the user for a string
+string = input("Enter a string: ")
+
 # convert the string to uppercase
 # print the string in uppercase
