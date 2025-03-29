b842d52b2ee26a86b1646d13f3b1d20b2a221417
Chariz <santoscharizkyle@gmail.com>
2025-03-29 22:55:23 +0800

    find the first occurrence of the substring

--- batch_7/prog09_finding_the_first_location_of_substring_without_using_index().py
+++ batch_7/prog09_finding_the_first_location_of_substring_without_using_index().py
@@ -6,4 +6,12 @@ string = input("Enter a string: ")
 substring = input("Enter the substring to find: ")
 
 # find the first occurrence of the substring using find()
+index = string.find(substring)
+
 # if the substring is found, print its index; otherwise, print a message
+if index != -1:
+    print()
+    print(f"The first location of '{substring}' is at index {index}.")
+else:
+    print()
+    print(f"'{substring}' was not found in the string.")
