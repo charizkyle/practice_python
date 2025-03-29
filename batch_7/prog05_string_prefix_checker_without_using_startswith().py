159628f312c27754bb6f228c8b4c25ac2820d058
Chariz <santoscharizkyle@gmail.com>
2025-03-29 21:05:44 +0800

    string input

--- batch_7/prog05_string_prefix_checker_without_using_startswith().py
+++ batch_7/prog05_string_prefix_checker_without_using_startswith().py
@@ -1,4 +1,8 @@
 # display the program's title
+print("Program 5: String Prefix Checker without using startswith()")
+
 # ask the user for a string and a prefix to check
+string = input("Enter a string: ")
+
 # check if the string starts with the given prefix
 # print the result
