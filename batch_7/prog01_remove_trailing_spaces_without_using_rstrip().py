30783d7d1e0c491c7f6bd3b399fca80b5e2e00c1
Chariz <santoscharizkyle@gmail.com>
2025-03-29 19:29:22 +0800

    input string

--- batch_7/prog01_remove_trailing_spaces_without_using_rstrip().py
+++ batch_7/prog01_remove_trailing_spaces_without_using_rstrip().py
@@ -1,4 +1,8 @@
 # display the program title
+print("Program 1: Remove Trailing Spaces from String without using rstrip()")
+
 # ask for usere input
+string = input("Enter a string with trailing spaces: ")
+
 # remove trailing spaces
 # print result
