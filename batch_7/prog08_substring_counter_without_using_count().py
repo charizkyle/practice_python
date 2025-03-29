4513ac8dded8a67bcb2f09171e28356a7a12841b
Chariz <santoscharizkyle@gmail.com>
2025-03-29 22:39:45 +0800

    count the occurrences of the substring in the main string

--- batch_7/prog08_substring_counter_without_using_count().py
+++ batch_7/prog08_substring_counter_without_using_count().py
@@ -6,4 +6,10 @@ main_string = input("Enter the main string: ")
 substring = input("Enter the substring to count: ")
 
 # count the occurrences of the substring in the main string
+count = 0
+index = main_string.find(substring)
+while index != -1:
+    count += 1
+    index = main_string.find(substring, index + 1)
+
 # display the result
