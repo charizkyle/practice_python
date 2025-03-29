892f1928f83f9f885ec077976a0d0da7c9608d9f
Chariz <santoscharizkyle@gmail.com>
2025-03-29 22:15:05 +0800

    user input: string and target length

--- batch_7/prog07_adding_zeros_at_the_beginning_without_using_zfill().py
+++ batch_7/prog07_adding_zeros_at_the_beginning_without_using_zfill().py
@@ -1,4 +1,9 @@
 # display the program's title
+print("Program 7: Adding Zeros at the Beginning without using zfill()")
+
 # input from user
+string = input("Enter a string: ")
+target_length = int(input("Enter the target length: "))
+
 # add leading zeros using string formatting
 # display the result
