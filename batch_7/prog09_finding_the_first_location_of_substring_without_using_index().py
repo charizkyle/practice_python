59eb6623f7be83af5f51363d13d3d40c94b99b21
Chariz <santoscharizkyle@gmail.com>
2025-03-29 22:49:39 +0800

    b7 pseudocode 9

---
+++ batch_7/prog09_finding_the_first_location_of_substring_without_using_index().py
@@ -0,0 +1,4 @@
+# display the program's title
+# ask the user for a string and a substring
+# find the first occurrence of the substring using find()
+# if the substring is found, print its index; otherwise, print a message
