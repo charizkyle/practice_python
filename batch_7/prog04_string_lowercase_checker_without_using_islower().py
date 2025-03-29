830eb75b832e9cea93b8667bc9f75dac07c03d56
Chariz <santoscharizkyle@gmail.com>
2025-03-29 20:50:18 +0800

    string input

--- batch_7/prog04_string_lowercase_checker_without_using_islower().py
+++ batch_7/prog04_string_lowercase_checker_without_using_islower().py
@@ -1,4 +1,8 @@
 # display the program's title
+print("Program 4: String Lowercase Checker without using islower()")
+
 # ask the user for a string
+string = input("Enter a string: ")
+
 # convert the string to lowercase and compare it with the original    
 # print the result
