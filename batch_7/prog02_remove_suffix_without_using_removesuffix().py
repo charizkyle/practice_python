12339ad702811b878018c9edceac243e6b1cd387
Chariz <santoscharizkyle@gmail.com>
2025-03-29 19:46:55 +0800

    b7 pseudocode 2

--- batch_7/prog02_remove_suffix_without_using_removesuffix().py
+++ batch_7/prog02_remove_suffix_without_using_removesuffix().py
@@ -1,16 +1,5 @@
 # display the program title
-print("Program 2: Remove Suffix from String without using removesuffix()")
-
 # input string and suffix to remove
-string = input("Enter a string: ")
-suffix = input("Enter the suffix to remove: ")
-
 # remove the suffix from the string without using removesuffix()
-if string.endswith(suffix):
-    string = string.replace(suffix, "", 1)
-else:
-    string = string # if no suffix is found, print the original string
-
+#   if no suffix is found, print the original string
 # print the string without the suffix
-print()
-print(string)
