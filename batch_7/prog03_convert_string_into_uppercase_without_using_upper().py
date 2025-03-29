77103d00feb5f9ce1b9d14ef94fd9371384603f6
Chariz <santoscharizkyle@gmail.com>
2025-03-29 20:36:36 +0800

    convert into uppercase

--- batch_7/prog03_convert_string_into_uppercase_without_using_upper().py
+++ batch_7/prog03_convert_string_into_uppercase_without_using_upper().py
@@ -4,5 +4,13 @@ print("Program 3: Convert String into Uppercase without using upper()")
 # ask the user for a string
 string = input("Enter a string: ")
 
-# convert the string to uppercase
-# print the string in uppercase
+result = ""
+for char in string:
+    if 'a' <= char <= 'z':
+        result += chr(ord(char) - 32)  # convert to uppercase
+    else:
+        result += char  # keep the character as is
+
+# print the result
+print()
+print(result)
