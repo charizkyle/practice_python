05c726c8bf1fdf0c4316e506fe081c8ef222e187
Chariz <santoscharizkyle@gmail.com>
2025-03-29 22:03:01 +0800

    string and target length input

--- batch_7/prog06_string_left_spacer_without_using_rjust().py
+++ batch_7/prog06_string_left_spacer_without_using_rjust().py
@@ -1,5 +1,10 @@
 # display the program's title
+print("Program 6: String Left Spacer without using rjust()")
+
 # ask the user for a string and a target length
+string = input("Enter a string: ")
+target_length = int(input("Enter the target length: "))
+
 # add spaces to the left of the string until it reaches the target length
 #   truncate the string if it is longer than the target length
 # print the result
