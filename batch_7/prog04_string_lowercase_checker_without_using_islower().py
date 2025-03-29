98defa9c4d40b8dc43f48319d3c643b62b8e61a2
Chariz <santoscharizkyle@gmail.com>
2025-03-29 20:54:59 +0800

    check if string is in lowercase

--- batch_7/prog04_string_lowercase_checker_without_using_islower().py
+++ batch_7/prog04_string_lowercase_checker_without_using_islower().py
@@ -4,5 +4,13 @@ print("Program 4: String Lowercase Checker without using islower()")
 # ask the user for a string
 string = input("Enter a string: ")
 
-# convert the string to lowercase and compare it with the original    
+# convert the string to lowercase and compare it with the original
+lowercase_string = string.lower()
+if string == lowercase_string:
+    result = "The string is in lowercase."
+else:
+    result = "The string is not in lowercase."
+
 # print the result
+print()
+print(result)
